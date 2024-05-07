97f9ab9eead4dc0dfee257648e1e6f3a30e1fc66
ja50n <zs_feng@qq.com>
2024-05-07 16:41:35 +0800

    done lab2.2

--- src/FunctionLayer/Texture/Texture.cpp
+++ src/FunctionLayer/Texture/Texture.cpp
@@ -1,7 +1,25 @@
 #include "Texture.h"
 
 TextureCoord UVMapping::map(const Intersection &intersection) const {
-  return TextureCoord{intersection.texCoord,
+  Vector2f coord = intersection.texCoord;
+  /// @brief Sphere texture mapping.
+  // Vector3f np = normalize(intersection.position - Point3f{0, 0, 0});
+  // float phi = fm::acos(np[2]), theta = fm::atan(np[1] / np[0]);
+  // float u = theta * 0.5 * INV_PI, v = phi * INV_PI;
+  // coord[0] = fm::fmod(u, 1.f), coord[1] = fm::fmod(v, 1.f);
+  /// @brief Cylinder texture mapping.
+  // Vector3f np = normalize(intersection.position - Point3f{0, 0, 0});
+  // float phi = fm::acos(np[2]), theta = fm::atan(np[1] / np[0]);
+  // float u = theta * 0.5 * INV_PI, v = np[2] >= 0 ? np[2] : np[2] + 1;
+  // coord[0] = fm::fmod(u, 1.f), coord[1] = fm::fmod(v, 1.f);
+  /// @brief Flat texture mapping.
+  // Vector3f v0{1, 0, 0}, v1{0, 1, 0}, p = intersection.position - Point3f{0, 0, 0};
+  // float u = dot(v0, p), v = dot(v1, p);
+  // coord[0] = fm::fmod(u, 1.f), coord[1] = fm::fmod(v, 1.f);
+
+  coord[0] += coord[0] < 0;
+  coord[1] += coord[1] < 0;
+  return TextureCoord{coord,
                       Vector2f{intersection.dudx, intersection.dvdx},
                       Vector2f{intersection.dudy, intersection.dvdy}};
 }
